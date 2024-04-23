c6b12e940a08f166fa3bce8eebfc0609a13a13cb
Trent Tompkins <TrentTompkins@gmail.com>
2024-04-22 22:36:15 -0400

    Add bootstrap

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
   <title></title>
   <link rel="stylesheet" href="css/style.css">
   <meta name="description" content="">
+  <link rel="" href="/style/bootstrap.css">
 
   <meta property="og:title" content="">
   <meta property="og:type" content="">
   <meta property="og:url" content="">
   <meta property="og:image" content="">
-
+  <link rel="stylesheet" href=" ">
   <link rel="icon" href="/favicon.ico" sizes="any">
   <link rel="icon" href="/icon.svg" type="image/svg+xml">
   <link rel="apple-touch-icon" href="icon.png">
@@ -22,11 +23,20 @@
 </head>
 
 <body>
+  <ul id="firstList" class="bootstrap"></ul>
 
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
-  <script src="js/app.js"></script>
+  <?php
+    phpinfo();
+  ?>
 
 </body>
+  <div id="container">
+    <span id="p1"></span><span id="p2"></span>
+    option#opt[name="$",value="$"]{$baseFile}*10
+
+  </div>
+<footer id="footer">
+  <script href="/js/app.js">
+</footer>
 
 </html>
